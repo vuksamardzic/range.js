4caf1271ee68f8354a969cd6f0ac4281111500ba
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-16 23:59:20 +0200

    webpack dev config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
-const path = require('path');
-const env = require('yargs').argv.env;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const mode = require('yargs').argv.mode
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const libname = 'range';
-const filename = 'range.min.js';
-const dir = 'lib';
-let plugins = [];
+const libname = 'range'
+const filename = 'range.min.js'
+const dir = 'lib'
+let plugins = []
+
+if (mode === 'development') {
+  plugins.push(new HtmlWebpackPlugin({
+    template: './index.html'
+  }))
+}
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, dir),
-        filename: filename,
-        library: 'range',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-    },
-    plugins: plugins
-};
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, dir),
+    filename: filename,
+    library: libname,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  plugins: plugins
+}
